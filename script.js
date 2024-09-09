d51fe86c6c56968eb8e8984e69a9585c414df12e
rahman <rahman17wr@gmail.com>
2024-09-09 23:15:56 +0700

    Get element from DOM

---
+++ script.js
@@ -0,0 +1,14 @@
+const newBookBtn = document.getElementById("new-book-btn");
+
+const readContainer = document.querySelector(".read-container");
+const notReadContainer = document.querySelector(".not-read-container");
+
+const modal = document.querySelector(".modal");
+const modalCancelBtn = document.querySelector("modal");
+const bookForm = document.getElementById("book-form");
+const bookTitle = document.getElementById("title");
+const bookAuthor = document.getElementById("author");
+const bookPages = document.getElementById("pages");
+const bookStatus = document.getElementById("status");
+
+
